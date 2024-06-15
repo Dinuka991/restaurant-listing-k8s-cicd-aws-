cedc4313f7287f478ea9dc21413a10fe65e64487
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-06-15 09:03:51 +0800

    Added the entity

---
+++ src/main/java/org/dinuka/resturantlistingk8scicdaws/ResturantListingK8sCicdAwsApplication.java
@@ -0,0 +1,13 @@
+package org.dinuka.resturantlistingk8scicdaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantListingK8sCicdAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResturantListingK8sCicdAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dinuka/resturantlistingk8scicdaws/ResturantListingK8sCicdAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dinuka.resturantlistingk8scicdaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantListingK8sCicdAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
